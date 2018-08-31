a3a47a3c90c866890c1871de569e8ab1a48e60b3
adi-howdy <adityakhatri123@gmail.com>
2018-08-30 20:46:21 -0700

    adding insert/find/update/drop in operation.js and calling it in index.js

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 
+const dboper = require('./operation');
+
 
 const url = 'mongodb://localhost:27017/conFusion';
 const dbname = 'conFusion1';
 
-MongoClient.connect(url, (err, client)=> {
-    assert.equal(err,null);
+MongoClient.connect(url, (err, client) => {
+    assert.equal(err, null);
     console.log('connected to server');
-
     const db = client.db(dbname);
+    /* *****************************
+   
     const collection = db.collection("dishes");
     collection.insertOne({"name":"Uthappizza", "description":"test"},
     (err, result) => {
@@ -32,4 +35,34 @@ MongoClient.connect(url, (err, client)=> {
             
         });
     });
+*************************************************/
+
+    dboper.insertDocument(db, { name: "adi", description: "hello" }, "dishes", (result) => {
+        console.log("Inserted document: " + result.ops);
+        dboper.findDocuments(db, "dishes", (docs) => {
+            console.log("found document ", docs);
+
+
+            dboper.updateDocument(db, { "name": "adi" }, { "name": "user" }, "dishes", (result) => {
+
+                console.log("updated  ::::::: ", result.result);
+
+                db.dropCollection("dishes", (result) => {
+                    console.log("drop collecito", result);
+
+                    client.close();
+                    console.log("db closed");
+                });
+            });
+        });
+    });
+
+
+
+
+
+
+
+
+
 });
